901bf08fd4c9afe49422463e4146eea7dd75d8e4
left-alert-chaos <left-alert-chaos@gmail.com>
2026-07-31 14:48:03 -0400

    Add Mod type to represent modules in code

--- src/object/builtins.rs
+++ src/object/builtins.rs
@@ -1,6 +1,7 @@
 //! # builtins
 //! This module holds object types that come with the interpreter such as `Executable` and `Mod`.
 //! When the AST creates an executable block or a Rust library wants to wrap a function, they use
+//! `Executable`. Ile-accessible functions are represented with `Object::Function`, which wraps
 //! `Executable`.
 
 use super::{Object, DataType};
@@ -13,6 +14,7 @@ pub type FunctionSignature<'a> = Vec<Object<'a>>;
 #[derive(Clone)]
 pub enum Executable<'a> {
     /// An executable object that is composed of Ile statements (yet to be implemented).
+    // TODO
     CodeBlock,
 
     /// A wrapper around a Rust function that is callable by Ile code.
@@ -20,4 +22,20 @@ pub enum Executable<'a> {
         signature: FunctionSignature<'a>,
         func: &'a Box<dyn Fn(FunctionSignature<'a>) -> Object>,
     },
+}
+
+pub struct Mod;
+
+impl<'a> DataType<'a> for Mod {
+    fn name(&self) -> String {
+        String::from("Mod")
+    }
+
+    fn methods(&self) -> std::collections::HashMap<&str, Object<'a>> {
+        std::collections::HashMap::new()
+    }
+
+    fn attributes(&self) -> std::collections::HashMap<&str, Object<'a>> {
+        std::collections::HashMap::new()
+    }
 }
