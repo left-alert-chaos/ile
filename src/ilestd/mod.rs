40b2fbba3c661adb86f6a029217a3d87029bee43
left-alert-chaos <left-alert-chaos@gmail.com>
2026-08-17 12:16:05 -0400

    Move ilestd to src

---
+++ src/ilestd/mod.rs
@@ -0,0 +1,3 @@
+//! # ilestd
+//! This module holds the source code of Ile's standard library. Some of it is in Rust, and some is
+//! in Ile.
--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ pub mod error;
 pub mod object;
 pub mod scope;
 pub mod interface;
+mod ilestd;
 
 pub use ast::*;
 pub use error::*;
 pub use object::*;
 pub use scope::*;
 pub use interface::*;
+use ilestd::*;
